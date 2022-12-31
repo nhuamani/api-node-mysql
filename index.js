e85330209b51f4134a24368e206f8d64f99f9c59
nhuamani <nhuamani.c7@gmail.com>
2022-12-30 20:56:44 -0500

    rutas de la aplicacion

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import express from 'express'
 
 
 const app = express()
+
+app.get('/employee', (req, res) => res.send('obteniendo empleados'))
+app.post('/employee', (req, res) => res.send('creando empleados'))
+app.put('/employee', (req, res) => res.send('actualizando empleados'))
+app.delete('/employee', (req, res) => res.send('eliminando empleados'))
+
 app.listen(3000)
