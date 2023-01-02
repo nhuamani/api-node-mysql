d696630ce8a251f007ddf31a6b42c995a147d209
nhuamani <nhuamani.c7@gmail.com>
2023-01-02 17:12:23 -0500

    configuracion de varibles de entorno

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import employeeRoutes from './routes/employee.routes.js'
+import indexRoutes from './routes/index.routes.js'
+
+
+const app = express()
+
+app.use(express.json())
+
+app.use(indexRoutes)
+app.use('/api', employeeRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Page not found'
+    })
+})
+
+export default app
--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
-import express from 'express'
-import employeeRoutes from './routes/employee.routes.js'
-import indexRoutes from './routes/index.routes.js'
+import app from './app.js'
+import {PORT} from './config.js'
 
 
-const app = express()
-
-app.use(express.json())
-
-app.use(indexRoutes)
-app.use('/api', employeeRoutes)
-app.use((req, res, next) => {
-    res.status(404).json({
-        message: 'Page not found'
-    })
+app.listen(PORT, () => {
+    console.log(`Server app listening on http://localhost:${PORT}`)
 })
-
-app.listen(3000)
