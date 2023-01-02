b049fba1dadf6d7cc23009fd357e39b0863d6e69
nhuamani <nhuamani.c7@gmail.com>
2023-01-02 16:38:39 -0500

    page not found

--- src/index.js
+++ src/index.js
@@ -9,5 +9,10 @@ app.use(express.json())
 
 app.use(indexRoutes)
 app.use('/api', employeeRoutes)
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Page not found'
+    })
+})
 
 app.listen(3000)
